baf73feb0d888b19279c33fe032829a684b3155f
Denver12345 <villaan12345>
2022-08-31 19:16:14 +0300

    day 2

--- src/components/Header/Choises/Choises.jsx
+++ src/components/Header/Choises/Choises.jsx
@@ -1,8 +1,47 @@
 import style from './Choises.module.css';
+import {useState} from "react";
 
-const Choises = () => (
 
-    <button className={style.button}> Выбрать праздник </button>
-);
+const holidays = {
+    newYear :'Новый год',
+    birthdayWomen:'День рождения  Ж',
+    birthdayMen:'День рождения  М',
+    womanDay:'8 марта',
+    knowledgeDay:'День знаний',
+}
+
+const Choises = () => {
+    const [isOpenChoices, setIsOpenChoices] = useState(false);
+    const [holiday, setHoliday] = useState('Выбрать праздник');
+
+    const toggleChoices = () =>{
+        setIsOpenChoices(!isOpenChoices)
+    };
+
+    const changeHoliday = title => {
+        setHoliday(title);
+        toggleChoices();
+    };
+
+
+    return (
+        <div className={style.wrapper}>
+            <button className={style.button} onClick={toggleChoices}> {holiday} </button>
+            { isOpenChoices && (
+                    <ul className={style.list}>
+                        {Object.entries(holidays).map(item => (
+                            <li className={style.item} key={item[0]} onClick={() => {
+                                changeHoliday(item[1])
+                            }}>
+                                {item[1]}
+                            </li>
+                        ))}
+                    </ul>
+                )
+            }
+
+        </div>
+    )
+};
 
 export default Choises;
