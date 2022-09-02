80e9fb4d116a0b2a9b147c30afc5bc8ad5d806da
Denver12345 <villaan12345>
2022-09-02 17:01:15 +0300

    day 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Card from "./components/Card/Card";
-import {TextContextProvider} from "./context/textContext";
 import {ImgContextProvider} from "./context/imgContext";
-import {HolidaysContextProvider} from "./context/holidaysContext";
+
 
 const wrapper = {
     display: 'flex',
@@ -15,15 +14,12 @@ const wrapper = {
 const App = () => {
   return (
     <div style={wrapper}>
-        <HolidaysContextProvider>
-            <ImgContextProvider>
-                <TextContextProvider>
-                    <Header />
-                    <Card />
-                    <Footer />
-                </TextContextProvider>
-            </ImgContextProvider>
-        </HolidaysContextProvider>
+             <ImgContextProvider>
+                 <Header />
+                 <Card />
+                 <Footer />
+             </ImgContextProvider>
+
     </div>
   );
 }
